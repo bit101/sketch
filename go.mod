8c1144cfa8e6e1bcfbfe40f5bbcfaab8f091be0b
Keith Peters <kp@bit-101.com>
2022-11-20 13:35:40 -0500

    fix commit location

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/bit101/sketch
+
+go 1.19
+
+require (
+	github.com/bit101/bitlib v0.1.0
+	github.com/bit101/blgg v0.2.6
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	golang.org/x/image v0.0.0-20220617043117-41969df76e82
+)
+
+require github.com/fogleman/gg v1.3.0 // indirect
---
+++ sketch.go
@@ -0,0 +1,134 @@
+// Package sketch draws sketchy shapes.
+package sketch
+
+import (
+	"math"
+
+	"github.com/bit101/bitlib/random"
+	"github.com/bit101/blgg"
+)
+
+// Sketch is a drawing context that draws sketchy shapes.
+type Sketch struct {
+	*blgg.Context
+	SegmentSize float64
+	Shake       float64
+}
+
+// NewSketch creates a new Sketch drawing context.
+func NewSketch(w, h float64) *Sketch {
+	c := blgg.NewContextF(w, h)
+	return FromContext(c)
+}
+
+// FromContext creates a sketch from a blgg context.
+func FromContext(context *blgg.Context) *Sketch {
+	return &Sketch{
+		context,
+		15,
+		5,
+	}
+}
+
+// MoveTo moves to the given x, y location, sketchily.
+func (s *Sketch) MoveTo(x, y float64) {
+	d := s.Shake / 2
+	s.Context.MoveTo(x+random.FloatRange(-d, d), y+random.FloatRange(-d, d))
+}
+
+// LineTo draws a sketchy line to the given x, y location.
+func (s *Sketch) LineTo(x, y float64) {
+	p, _ := s.GetCurrentPoint()
+	d := s.Shake / 2
+	endX := x + random.FloatRange(-d, d)
+	endY := y + random.FloatRange(-d, d)
+	dx := endX - p.X
+	dy := endY - p.Y
+	dist := math.Hypot(dx, dy)
+	steps := math.Floor(dist / s.SegmentSize)
+	resX := dx / steps
+	resY := dy / steps
+	for i := 1.0; i < steps; i++ {
+		s.Context.LineTo(p.X+resX*i+random.FloatRange(-d, d), p.Y+resY*i+random.FloatRange(-d, d))
+	}
+	s.Context.LineTo(endX, endY)
+}
+
+// StrokeMultiLine draws multiple, offset stroked lines between two points.
+func (s *Sketch) StrokeMultiLine(x0, y0, x1, y1, separation float64, iter int) {
+	for i := 0; i < iter; i++ {
+		s.MoveTo(x0+random.FloatRange(-separation, separation), y0+random.FloatRange(-separation, separation))
+		s.LineTo(x1+random.FloatRange(-separation, separation), y1+random.FloatRange(-separation, separation))
+		s.Stroke()
+	}
+}
+
+// Circle draws a sketchy circle.
+func (s *Sketch) Circle(xc, yc, r float64) {
+	d := s.Shake / 2
+	circ := r * math.Pi * 2
+	steps := circ / s.SegmentSize
+	res := math.Pi * 2 / steps
+	for a := 0.0; a < math.Pi*2; a += res {
+		x := xc + math.Cos(a)*r + random.FloatRange(-d, d)
+		y := yc + math.Sin(a)*r + random.FloatRange(-d, d)
+		s.Context.LineTo(x, y)
+	}
+	s.ClosePath()
+}
+
+// StrokeCircle strokes a sketchy circle.
+func (s *Sketch) StrokeCircle(xc, yc, r float64) {
+	s.Circle(xc, yc, r)
+	s.Stroke()
+}
+
+// FillCircle fills a sketchy circle.
+func (s *Sketch) FillCircle(xc, yc, r float64) {
+	s.Circle(xc, yc, r)
+	s.Fill()
+}
+
+// Rectangle draws a sketchy rectangle.
+func (s *Sketch) Rectangle(x, y, w, h float64) {
+	s.MoveTo(x, y)
+	s.LineTo(x+w, y)
+	s.LineTo(x+w, y+h)
+	s.LineTo(x, y+h)
+	s.LineTo(x, y)
+}
+
+// StrokeRectangle strokes a sketchy rectangle.
+func (s *Sketch) StrokeRectangle(x, y, w, h float64) {
+	s.Rectangle(x, y, w, h)
+	s.Stroke()
+}
+
+// FillRectangle fills a sketchy rectangle.
+func (s *Sketch) FillRectangle(x, y, w, h float64) {
+	s.Rectangle(x, y, w, h)
+	s.Fill()
+}
+
+// StrokeMultiRect strokes multiple sketchy rectangles
+func (s *Sketch) StrokeMultiRect(x, y, w, h, separation float64, iter int) {
+	s.StrokeMultiLine(x, y, x+w, y, separation, iter)
+	s.StrokeMultiLine(x+w, y, x+w, y+h, separation, iter)
+	s.StrokeMultiLine(x+w, y+h, x, y+h, separation, iter)
+	s.StrokeMultiLine(x, y+h, x, y, separation, iter)
+}
+
+// DrawString draws a sketchy string
+func (s *Sketch) DrawString(str string, x, y float64) {
+	d := s.Shake / 2
+	rot := d * 0.05
+	for _, c := range str {
+		s.Push()
+		s.Translate(x, y)
+		s.Rotate(random.FloatRange(-rot, rot))
+		s.Context.DrawString(string(c), random.FloatRange(-d, d), random.FloatRange(-d, d))
+		w, _ := s.MeasureString(string(c))
+		x += w
+		s.Pop()
+	}
+}
